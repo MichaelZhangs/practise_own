fbdc7a69b4d538d0666e59398ff600f7ddcd56a8
MichaelZhangs <58901344+MichaelZhangs@users.noreply.github.com>
2021-03-06 11:11:06 +0800

    Add files via upload

---
+++ login_qq.py
@@ -0,0 +1,24 @@
+#encoding:utf8
+from selenium import webdriver
+import time
+
+from selenium.webdriver import ActionChains
+
+
+driver = webdriver.Chrome()
+driver.get("https://mail.qq.com/")
+time.sleep(4)
+#输入密码
+
+driver.maximize_window()
+
+driver.find_element_by_id("qqLoginTab").click()
+# iframe = driver.find_element_by_tag_name("iframe")
+# print(iframe)
+driver.switch_to.frame("login_frame")
+driver.find_element_by_id("u").send_keys("*********")
+time.sleep(2)
+driver.find_element_by_id("p").send_keys("************")
+
+driver.find_element_by_id("login_button").click()
+time.sleep(3)
